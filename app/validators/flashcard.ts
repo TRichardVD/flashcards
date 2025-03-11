26604101c16fd40d80bf1680d86c1d0e6c214322
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-03-11 22:44:43 +0100

    fix(validators, flashcards): Update minimum length requirement for recto and verso fields to 10 characters

--- app/validators/flashcard.ts
+++ app/validators/flashcard.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const FlashcardValidator = vine.compile(
   vine.object({
-    recto: vine.string().minLength(1).maxLength(1000),
-    verso: vine.string().minLength(1).maxLength(1000),
+    recto: vine.string().minLength(10).maxLength(1000),
+    verso: vine.string().minLength(10).maxLength(1000),
   })
 )
